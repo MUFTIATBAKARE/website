99475fa7109ebf56fcfd714409f6b4837209e110
Muftiat Bakare Abdullahi <105362326+MUFTIATBAKARE@users.noreply.github.com>
2023-03-26 00:59:01 +0100

    Update App.js
    Update: writing react

--- src/App.js
+++ src/App.js
@@ -4,20 +4,17 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>
+     <h1>Welcome to my website!</h1>
+     <nav>
+       <ul>
+         <li><a href="#">Home</a></li>
+         <li><a href="#">About</a></li>
+         <li><a href="#">Services</a></li>
+         <li><a href="#">Contact</a></li>
+       </ul>
+     </nav>
+   </header>
     </div>
   );
 }
