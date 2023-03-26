4207c618ed80929f2f6d888a3fc50f912cd53e42
Muftiat Bakare Abdullahi <105362326+MUFTIATBAKARE@users.noreply.github.com>
2023-03-26 01:05:44 +0100

    Update Header.js
    Update: header file

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 function Header() {
-  return <div>Header</div>;
+  return <div>
+<header>
+     <h1>Welcome to my website!</h1>
+     <nav>
+       <ul>
+         <li><a href="#">Home</a></li>
+         <li><a href="#">About</a></li>
+         <li><a href="#">Services</a></li>
+         <li><a href="#">Contact</a></li>
+       </ul>
+     </nav>
+   </header>
+</div>;
 }
 
 export default Header;
